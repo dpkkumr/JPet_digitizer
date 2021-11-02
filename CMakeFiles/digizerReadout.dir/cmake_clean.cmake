b96b62ba108968f739cb15719d04e5d40bfdb3fb
dpkkumr <64909325+dpkkumr@users.noreply.github.com>
2021-11-02 18:40:26 +0100

    Add files via upload

---
+++ CMakeFiles/digizerReadout.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/digizerReadout.dir/eventTools.cpp.o"
+  "CMakeFiles/digizerReadout.dir/eventTools.cpp.o.d"
+  "CMakeFiles/digizerReadout.dir/main.cpp.o"
+  "CMakeFiles/digizerReadout.dir/main.cpp.o.d"
+  "digizerReadout"
+  "digizerReadout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/digizerReadout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
